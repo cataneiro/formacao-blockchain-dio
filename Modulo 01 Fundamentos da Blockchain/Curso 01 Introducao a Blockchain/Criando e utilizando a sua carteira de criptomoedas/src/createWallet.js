c2e30e9885d077c18bb347db36045b121bff7ad2
Bruno Mota <souzabruno20@hotmail.com>
2024-09-10 14:22:41 -0300

    Update createWallet.js
    Foi feito algumas correções a do original para que as chaves pudessem ser criadas corretamente

--- Modulo 01 Fundamentos da Blockchain/Curso 01 Introducao a Blockchain/Criando e utilizando a sua carteira de criptomoedas/src/createWallet.js
+++ Modulo 01 Fundamentos da Blockchain/Curso 01 Introducao a Blockchain/Criando e utilizando a sua carteira de criptomoedas/src/createWallet.js
@@ -5,8 +5,19 @@ const bitcoin = require('bitcoinjs-lib')
 
 //definir a rede
 //bitcoin - rede principal - mainnet
-//testnet - rede de teste - tesnet
-const network = bitcoin.networks.testnet
+//testnet - rede de teste - tesnet4
+
+const testnet4 = {
+    messagePrefix: '\x18Bitcoin Signed Message:\n',
+    bech32: 'tb',
+    bip32: {
+      public: 0x043587cf,  // Base58 prefix for xpub on Testnet4
+      private: 0x04358394  // Base58 prefix for xprv on Testnet4
+    },
+    pubKeyHash: 0x6f,      // P2PKH starts with 'm' or 'n'
+    scriptHash: 0xc4,      // P2SH starts with '2'
+    wif: 0xef              // Wallet Import Format prefix
+  };
 
 //derivação de carteiras HD
 const path = `m/49'/1'/0'/0` 
@@ -16,7 +27,7 @@ let mnemonic = bip39.generateMnemonic()
 const seed = bip39.mnemonicToSeedSync(mnemonic)
 
 //criando a raiz da cartiera HD
-let root = bip32.fromSeed(seed, network)
+let root = bip32.fromSeed(seed, testnet4)
 
 //criando uma conta - par pvt-pub keys
 let account = root.derivePath(path)
@@ -24,10 +35,10 @@ let node = account.derive(0).derive(0)
 
 let btcAddress = bitcoin.payments.p2pkh({
     pubkey: node.publicKey,
-    network: network,
+    network: testnet4,
 }).address
 
-console.log("Carteira gerada")
+console.log("Carteira gerada: ")
 console.log("Endereço: ", btcAddress)
-console.log("Chave privada:", node.toWIF())
-console.log("Seed:", mnemonic)
+console.log("Chave privada: ", node.toWIF())
+console.log("Seed: ", mnemonic)
